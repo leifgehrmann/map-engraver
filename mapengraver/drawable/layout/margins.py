de7231513d95985803582ee0b786f2607438626f
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-03-07 23:08:28 +0000

    Implemented Margins

---
+++ mapengraver/drawable/layout/margins.py
@@ -0,0 +1,76 @@
+from shapely.geometry import Polygon
+from typing import Optional
+
+from mapengraver.canvas import Canvas
+from mapengraver.canvas.canvas_unit import CanvasUnit
+from mapengraver.drawable.drawable import Drawable
+from mapengraver.graphicshelper import CairoHelper
+from mapengraver.logging.progressable import ProgressObservable
+
+
+class Margins(Drawable, ProgressObservable):
+    stroke_width: Optional[CanvasUnit]
+    margin_bottom: Optional[CanvasUnit]
+    margin_top: Optional[CanvasUnit]
+    margin_right: Optional[CanvasUnit]
+    margin_left: Optional[CanvasUnit]
+
+    def __init__(self):
+        self.margin_left = None
+        self.margin_right = None
+        self.margin_top = None
+        self.margin_bottom = None
+        self.fill_color = (1, 1, 1, 1)
+        self.stroke_color = (0, 0, 0, 1)
+        self.stroke_width = None
+
+    def set_margins(self, margin: CanvasUnit):
+        self.margin_left = margin
+        self.margin_right = margin
+        self.margin_top = margin
+        self.margin_bottom = margin
+
+    def progress(self):
+        pass
+
+    def draw(self, canvas: Canvas):
+        min_x = self.margin_left.pt
+        min_y = self.margin_top.pt
+        max_x = canvas.width - self.margin_right.pt
+        max_y = canvas.height - self.margin_bottom.pt
+
+        exterior_shape = Polygon(
+            [
+                (0, 0),
+                (canvas.width, 0),
+                (canvas.width, canvas.height),
+                (0, canvas.height),
+                (0, 0)
+            ],
+            [
+                list(reversed([
+                    (min_x, min_y),
+                    (max_x, min_y),
+                    (max_x, max_y),
+                    (min_x, max_y),
+                    (min_x, min_y)
+                ]))
+            ]
+        )
+
+        if self.stroke_width is not None:
+            interior_outline = Polygon([
+                (min_x, min_y),
+                (max_x, min_y),
+                (max_x, max_y),
+                (min_x, max_y),
+                (min_x, min_y)
+            ])
+            canvas.context.set_line_width(self.stroke_width.pt)
+            canvas.context.set_source_rgba(*self.stroke_color)
+            CairoHelper.draw_polygon(canvas.context, interior_outline)
+            canvas.context.stroke()
+
+        canvas.context.set_source_rgba(*self.fill_color)
+        CairoHelper.draw_polygon(canvas.context, exterior_shape)
+        canvas.context.fill()
---
+++ tests/drawable/layout/test_margins.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import unittest
+
+from mapengraver.canvas import CanvasBuilder
+from mapengraver.canvas.canvas_unit import CanvasUnit
+from mapengraver.drawable.layout.margins import Margins
+
+
+class TestMargins(unittest.TestCase):
+    def setUp(self):
+        Path(__file__).parent.joinpath('output/') \
+            .mkdir(parents=True, exist_ok=True)
+
+    def test_output(self):
+        path = Path(__file__).parent.joinpath('output/margins.svg')
+        path.unlink(missing_ok=True)
+        canvas_builder = CanvasBuilder()
+        canvas_builder.set_path(path)
+        canvas_builder.set_size(20, 15, 'cm')
+
+        canvas = canvas_builder.build()
+
+        margins = Margins()
+        margins.set_margins(CanvasUnit.from_cm(3))
+        margins.margin_top = CanvasUnit.from_cm(2)
+        margins.margin_bottom = CanvasUnit.from_cm(2)
+        margins.stroke_width = CanvasUnit.from_mm(1)
+        margins.stroke_color = (0.5, 0.5, 0.1, 1)
+        margins.fill_color = (1, 1, 0.9, 1)
+        margins.draw(canvas)
+
+        canvas.close()
+
+        assert path.exists()
