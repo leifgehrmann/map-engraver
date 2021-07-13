cf6d9f4d699ce14c018864ce41a366826db9584d
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-07-13 22:11:34 +0100

    Even more test cases

--- tests/transformers/test_osm_to_shapely.py
+++ tests/transformers/test_osm_to_shapely.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import unittest
 
 from mapengraver.data.osm import Parser
-from mapengraver.transformers.osm_to_shapely import OsmToShapely
+from mapengraver.transformers.osm_to_shapely import OsmToShapely, WayToPolygonError
 
 
 class TestOsmToShapely(unittest.TestCase):
@@ -17,41 +17,63 @@ class TestOsmToShapely(unittest.TestCase):
         highway_service_linestring = osm_to_shapely.way_to_linestring(
             highway_service_way
         )
-
+        assert highway_service_linestring.osm_tags['highway'] == 'service'
         assert list(highway_service_linestring.coords) == [
             (5.71135378422, 59.00609795619),
             (5.71135378422, 59.01678200227),
             (5.72491834501, 59.01675888021)
         ]
-        assert highway_service_linestring.osm_tags['highway'] == 'service'
 
-        bank_building_way = osm_map.get_way('-101787')
-        bank_building_polygon = osm_to_shapely.way_to_polygon(bank_building_way)
-
-        assert list(bank_building_polygon.exterior.coords) == [
+        cw_bank_building_way = osm_map.get_way('-101787')
+        cw_bank_building_polygon = osm_to_shapely.way_to_polygon(cw_bank_building_way)
+        assert cw_bank_building_polygon.osm_tags['building'] == 'yes'
+        assert list(cw_bank_building_polygon.exterior.coords) == [
             (5.71666872951, 59.01328584519), (5.73319773074, 59.01333209396),
             (5.73327340446, 59.00616275303), (5.71674440323, 59.00611649462),
             (5.71666872951, 59.01328584519)
         ]
-        assert bank_building_polygon.osm_tags['building'] == 'yes'
 
-        building_relation = osm_map.get_relation('-99750')
-        building_polygon = osm_to_shapely.relation_to_polygon(building_relation)
-        assert list(building_polygon.exterior.coords) == [
+        ccw_bank_building_way = osm_map.get_way('-102178')
+        ccw_bank_building_polygon = osm_to_shapely.way_to_polygon(ccw_bank_building_way)
+        assert ccw_bank_building_polygon.osm_tags['building'] == 'yes'
+        assert list(ccw_bank_building_polygon.exterior.coords) == [
+            (5.71703365152, 59.0023220992), (5.73356265275, 59.00236836271),
+            (5.73363832647, 58.99519673761), (5.71710932524, 58.99515046446),
+            (5.71703365152, 59.0023220992)
+        ]
+
+        cw_building_relation = osm_map.get_relation('-99750')
+        cw_building_polygon = osm_to_shapely.relation_to_polygon(cw_building_relation)
+        assert cw_building_polygon.osm_tags['building'] == 'yes'
+        assert list(cw_building_polygon.exterior.coords) == [
             (5.73921644314, 59.01319334747), (5.75349906139, 59.01296272491),
             (5.75341159278, 59.00625219859), (5.73912661162, 59.0063015675),
             (5.73921644314, 59.01319334747)
         ]
-        assert list(building_polygon.interiors[0].coords) == [
+        assert list(cw_building_polygon.interiors[0].coords) == [
             (5.74469616638, 59.01051080523), (5.74469616638, 59.00847563368),
             (5.7491877428, 59.00847563368), (5.7491877428, 59.01051080523),
             (5.74469616638, 59.01051080523)
         ]
-        assert building_polygon.osm_tags['building'] == 'yes'
+
+        ccw_building_relation = osm_map.get_relation('-99893')
+        ccw_building_polygon = osm_to_shapely.relation_to_polygon(ccw_building_relation)
+        assert ccw_building_polygon.osm_tags['building'] == 'yes'
+        assert list(ccw_building_polygon.exterior.coords) == [
+            (5.73956674445, 59.00198690218), (5.75385165323, 59.00189041861),
+            (5.75367843649, 58.99508762548), (5.73939352771, 58.99518412812),
+            (5.73956674445, 59.00198690218)
+        ]
+        assert list(ccw_building_polygon.interiors[0].coords) == [
+            (5.74500532962, 58.99937088385), (5.74500532962, 58.99733505352),
+            (5.74949690604, 58.99733505352), (5.74949690604, 58.99937088385),
+            (5.74500532962, 58.99937088385)
+        ]
+
 
         water_relation = osm_map.get_relation('-99778')
         water_polygon = osm_to_shapely.relation_to_polygon(water_relation)
-        print(list(water_polygon.exterior.coords))
+        assert water_polygon.osm_tags['natural'] == 'water'
         assert list(water_polygon.exterior.coords) == [
             (5.77204331465, 59.0082363528), (5.7695647063, 59.00678458244),
             (5.76627727032, 59.00629616253), (5.76186221583, 59.00717558346),
@@ -60,5 +82,28 @@ class TestOsmToShapely(unittest.TestCase):
             (5.76752463333, 59.01281118926), (5.77081092363, 59.0118769482),
             (5.77251263289, 59.01015411586), (5.77204331465, 59.0082363528)
         ]
-        assert water_polygon.osm_tags['natural'] == 'water'
+
+    def test_conversion_fails_for_invalid_types(self):
+        path = Path(__file__).parent.joinpath('data.osm')
+
+        osm_map = Parser.parse(path)
+        osm_to_shapely = OsmToShapely(osm_map)
+
+        highway_service_way = osm_map.get_way('-101873')
+        with self.assertRaises(WayToPolygonError):
+            osm_to_shapely.way_to_polygon(highway_service_way)
+
+    def test_transform(self):
+        path = Path(__file__).parent.joinpath('data.osm')
+
+        osm_map = Parser.parse(path)
+        osm_to_shapely = OsmToShapely(osm_map, lambda x, y: (0, 0))
+
+        highway_service_way = osm_map.get_way('-101873')
+        highway_service_linestring = osm_to_shapely.way_to_linestring(
+            highway_service_way
+        )
+        assert list(highway_service_linestring.coords) == [
+            (0, 0), (0, 0), (0, 0)
+        ]
 
