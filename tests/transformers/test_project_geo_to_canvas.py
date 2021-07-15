33012d5f58e7f593c458dbbc9e7434a36b71c508
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-07-15 19:03:49 +0100

    Better text formatting

--- tests/transformers/test_project_geo_to_canvas.py
+++ tests/transformers/test_project_geo_to_canvas.py
@@ -18,19 +18,19 @@ class TestProjectGeoToCanvas(unittest.TestCase):
         if not math.isclose(expected_coordinate[0], actual_coordinate[0]) or \
                 not math.isclose(expected_coordinate[1], actual_coordinate[1]):
             raise AssertionError(
-                'Coordinates are not close. Expected:' +
+                'Coordinates are not close. Expected: (' +
                 str(expected_coordinate[0]) + ', ' +
                 str(expected_coordinate[1]) +
-                '. Actual: ' +
+                '). Actual: (' +
                 str(actual_coordinate[0]) + ', ' +
-                str(actual_coordinate[1])
+                str(actual_coordinate[1]) + ')'
             )
 
     def test_conversion(self):
         wgs_84_identity_func = build_projection_function()
         self.assert_coordinates_are_close(
             wgs_84_identity_func(55.855529, -4.232459),
-            (55.855529, -4.232459)
+            (55.855529, -5.232459)
         )
 
         british_grid_func = build_projection_function(
