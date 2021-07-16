dd5112bfeb01a8a691c9cb43081ac38cee6644da
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-07-16 12:07:42 +0100

    Improve testing, also added inversion function, but need more tests

--- mapengraver/transformers/project_geo_to_canvas.py
+++ mapengraver/transformers/project_geo_to_canvas.py
@@ -10,10 +10,37 @@ def build_projection_function(
         output_origin: Tuple[float, float] = (0, 0),
         canvas_origin: Tuple[float, float] = (0, 0),
         output_scale: float = 1,
-        canvas_scale: CanvasUnit = CanvasUnit(1)
+        canvas_scale: CanvasUnit = CanvasUnit(1),
+        inverse: bool = False
 ) -> Callable[[float, float], Tuple[float, float]]:
-    my_function = pyproj.Transformer.from_proj(
+    geo_projection = pyproj.Transformer.from_proj(
         input_crs,
         output_crs
     )
-    return lambda x, y: my_function.transform(x, y)
+    translate = (output_origin[0] - canvas_origin[0],
+                 output_origin[1] - canvas_origin[1])
+    scale = output_scale / canvas_scale.pt
+
+    if inverse:
+        geo_projection = pyproj.Transformer.from_proj(
+            output_crs,
+            input_crs
+        )
+
+        def inv_projection(x: float, y: float) -> Tuple[float, float]:
+            coord = (x / scale + translate[0], y / scale + translate[1])
+            return geo_projection.transform(coord[0], coord[1])
+
+        return inv_projection
+    else:
+        geo_projection = pyproj.Transformer.from_proj(
+            input_crs,
+            output_crs
+        )
+
+        def projection(x: float, y: float) -> Tuple[float, float]:
+            coord = geo_projection.transform(x, y)
+            translated = (coord[0] - translate[0], coord[1] - translate[1])
+            return translated[0] * scale, translated[1] * scale
+
+        return projection
--- tests/transformers/test_project_geo_to_canvas.py
+++ tests/transformers/test_project_geo_to_canvas.py
@@ -26,17 +26,50 @@ class TestProjectGeoToCanvas(unittest.TestCase):
                 str(actual_coordinate[1]) + ')'
             )
 
-    def test_conversion(self):
+    @staticmethod
+    def magnitude(v: Tuple[float, float]):
+        return math.sqrt(v[0] * v[0] + v[1] * v[1])
+
+    def test_identity_conversion(self):
         wgs_84_identity_func = build_projection_function()
         self.assert_coordinates_are_close(
             wgs_84_identity_func(55.855529, -4.232459),
-            (55.855529, -5.232459)
+            (55.855529, -4.232459)
         )
 
+    def test_british_national_grid_conversion(self):
         british_grid_func = build_projection_function(
+            # 27700 is the british national grid
             output_crs=pyproj.CRS.from_epsg(27700)
         )
         self.assert_coordinates_are_close(
+            # Glasgow
             british_grid_func(55.855529, -4.232459),
             (260354.7929476458, 664735.6993417306)
+        )
+
+    def test_offset_conversion(self):
+        wgs84_crs = pyproj.CRS.from_epsg(4326)
+        british_crs = pyproj.CRS.from_epsg(27700)
+        crs_transformer = pyproj.Transformer.from_proj(
+            wgs84_crs,
+            british_crs
+        )
+        walter_scott_monument_wgs84 = (55.86115, -4.25017)
+        glasgow_central_station_wgs84 = (55.86045, -4.25772)
+        walter_scott_column_british = crs_transformer.transform(
+            *walter_scott_monument_wgs84
+        )
+        glasgow_func = build_projection_function(
+            input_crs=wgs84_crs,
+            output_crs=british_crs,
+            output_origin=walter_scott_column_british
+        )
+        actual_distance_between_poi = math.floor(self.magnitude(
+            glasgow_func(*glasgow_central_station_wgs84)
+        ))
+        expected_distance_between_poi = 479
+        assert math.isclose(
+            actual_distance_between_poi,
+            expected_distance_between_poi
         )
