01d36da9bb4c1ab918ca94c0a1a63b818f57897c
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-06-08 21:39:58 +0100

    Fixed type-hints, added basic rendering for single-stripes

--- map_engraver/drawable/geometry/stripe_filled_polygon_drawer.py
+++ map_engraver/drawable/geometry/stripe_filled_polygon_drawer.py
@@ -3,32 +3,40 @@ from typing import List, Optional, Tuple, Union
 from shapely.geometry import Polygon, MultiPolygon
 
 from map_engraver.canvas import Canvas
+from map_engraver.canvas.canvas_coordinate import CanvasCoordinate
 from map_engraver.canvas.canvas_unit import CanvasUnit
 from map_engraver.drawable.drawable import Drawable
 from map_engraver.drawable.geometry.polygon_drawer import PolygonDrawer
 
 
 class StripeFilledPolygonDrawer(Drawable):
-    stroke_width: Optional[CanvasUnit]
-    width_arr: List[float]
+    width_arr: List[CanvasUnit]
     color_arr: List[Optional[Tuple[float, float, float, float]]]
-    origin: Tuple[float, float]
+    origin: CanvasCoordinate
     angle: float
     geoms: List[Union[Polygon, MultiPolygon]]
 
     def __init__(self):
         self.geoms = []
-        self.width_arr = [1]
-        self.color_arr = [(0, 0, 0, 1)]
+        self.stripe_widths = [CanvasUnit.from_pt(1)]
+        self.stripe_colors = [(0, 0, 0, 1)]
         self.angle = 0
-        self.origin = (0, 0)
+        self.origin = CanvasCoordinate.origin()
 
     def draw(self, canvas: Canvas):
-        if len(self.width_arr) != len(self.color_arr):
+        if len(self.stripe_widths) != len(self.stripe_colors):
             raise ValueError(
                 'length of width_arr is not the same as color_arr'
             )
-        polygon_drawer = PolygonDrawer()
-        polygon_drawer.fill_color = self.color_arr[0]
-        polygon_drawer.polygons = self.geoms
-        polygon_drawer.draw(canvas)
+        if len(self.stripe_widths) == 0:
+            raise ValueError(
+                'width_arr must contain at least one value'
+            )
+        if len(self.stripe_widths) == 1:
+            if self.stripe_colors[0] is None:
+                return
+            polygon_drawer = PolygonDrawer()
+            polygon_drawer.fill_color = self.stripe_colors[0]
+            polygon_drawer.geoms = self.geoms
+            polygon_drawer.draw(canvas)
+            return
