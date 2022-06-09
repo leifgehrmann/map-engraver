72d506b4247dee7b73878d8bfbe958c187fc5e2b
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-06-09 21:28:01 +0100

    Added basic math function

--- map_engraver/data/math/trig.py
+++ map_engraver/data/math/trig.py
@@ -4,6 +4,10 @@ from typing import Tuple
 
 Coord = Tuple[float, float]
 Vector = Tuple[float, float]
+UnitVector = Vector
+
+# A line is defined by two vectors, its origin, and its direction
+Line = Tuple[Coord, UnitVector]
 
 
 def _diff(a: Coord, b: Coord) -> Coord:
@@ -17,6 +21,10 @@ def _magnitude(v: Vector) -> float:
     return sqrt(v[0] * v[0] + v[1] * v[1])
 
 
+def _dot(a: Vector, b: Vector) -> float:
+    return a[0] * b[0] + a[1] * b[1]
+
+
 def obtuse_angle(a: Coord, b: Coord, c: Coord) -> float:
     """
     Given all three coordinates of a triangle, this function returns the
@@ -24,7 +32,7 @@ def obtuse_angle(a: Coord, b: Coord, c: Coord) -> float:
     """
     ca = _diff(c, a)
     cb = _diff(c, b)
-    ca_dot_bc = ca[0] * cb[0] + ca[1] * cb[1]
+    ca_dot_bc = _dot(ca, cb)
     mag_mul = _magnitude(ca) * _magnitude(cb)
     if mag_mul == 0:
         return float('inf')
@@ -36,3 +44,26 @@ def obtuse_angle(a: Coord, b: Coord, c: Coord) -> float:
         return 0
     angle_radians = acos(adj_hyp)
     return angle_radians / pi * 180
+
+
+def scalar_between_lines_origin_and_projected_point(
+    line: Line,
+    point: Coord
+) -> float:
+    """
+    Returns the scalar which represents the distance and direction between the
+    line's origin, and a point's projected position on the line. It's similar
+    to the "distance between a point and a line" problem, but instead it's the
+    point on the line and the line's origin.
+
+    Essentially the formula is: (a - p) · n
+
+    Wikimedia has a useful graphic that illustrates this.
+    https://commons.wikimedia.org/wiki/File:Distance_from_a_point_to_a_line.svg
+
+    :param line:
+    :param angle:
+    :param point:
+    :return:
+    """
+    return _dot(_diff(line[0], point), line[1])
--- tests/data/math/test_trig.py
+++ tests/data/math/test_trig.py
@@ -1,6 +1,9 @@
+import math
+
 import unittest
 
-from map_engraver.data.math.trig import obtuse_angle
+from map_engraver.data.math.trig import obtuse_angle, \
+    scalar_between_lines_origin_and_projected_point
 
 
 class TestTrig(unittest.TestCase):
@@ -19,3 +22,32 @@ class TestTrig(unittest.TestCase):
         self.assertAlmostEqual(obtuse_angle((1, 0), (-1, -1), (0, 0)), 135, 10)
         self.assertEqual(obtuse_angle((1, 0), (0, -1), (0, 0)), 90)
         self.assertAlmostEqual(obtuse_angle((1, 0), (1, -1), (0, 0)), 45, 10)
+
+    def test_scalar_between_lines_origin_and_projected_point(self):
+        line = ((2, 0), (0, 1))
+        self.assertEqual(
+            scalar_between_lines_origin_and_projected_point(line, (0, 1)),
+            1
+        )
+        self.assertEqual(
+            scalar_between_lines_origin_and_projected_point(line, (1, 2)),
+            2
+        )
+        self.assertEqual(
+            scalar_between_lines_origin_and_projected_point(line, (2, -1)),
+            -1
+        )
+        self.assertEqual(
+            scalar_between_lines_origin_and_projected_point(line, (3, -2)),
+            -2
+        )
+
+        line = ((1, 1), (math.sqrt(0.5), math.sqrt(0.5)))
+        self.assertEqual(
+            scalar_between_lines_origin_and_projected_point(line, (3, 1)),
+            math.sqrt(2)
+        )
+        self.assertEqual(
+            scalar_between_lines_origin_and_projected_point(line, (1, -1)),
+            -math.sqrt(2)
+        )
