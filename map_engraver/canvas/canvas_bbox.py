67137b8f86d92b890fa3c715d8dc9c3727bd1386
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-07-11 20:45:59 +0100

    Added more BBox methods

--- map_engraver/canvas/canvas_bbox.py
+++ map_engraver/canvas/canvas_bbox.py
@@ -16,3 +16,43 @@ class CanvasBbox:
         self.pos = pos
         self.width = width
         self.height = height
+
+    @classmethod
+    def from_pt(cls, x: float, y: float, w: float, h: float) -> 'CanvasBbox':
+        return CanvasBbox(
+            CanvasCoordinate.from_pt(x, y),
+            CanvasUnit.from_pt(w),
+            CanvasUnit.from_pt(h)
+        )
+
+    @classmethod
+    def from_px(cls, x: float, y: float, w: float, h: float) -> 'CanvasBbox':
+        return CanvasBbox(
+            CanvasCoordinate.from_px(x, y),
+            CanvasUnit.from_px(w),
+            CanvasUnit.from_px(h)
+        )
+
+    @classmethod
+    def from_in(cls, x: float, y: float, w: float, h: float) -> 'CanvasBbox':
+        return CanvasBbox(
+            CanvasCoordinate.from_in(x, y),
+            CanvasUnit.from_in(w),
+            CanvasUnit.from_in(h)
+        )
+
+    @classmethod
+    def from_cm(cls, x: float, y: float, w: float, h: float) -> 'CanvasBbox':
+        return CanvasBbox(
+            CanvasCoordinate.from_cm(x, y),
+            CanvasUnit.from_cm(w),
+            CanvasUnit.from_cm(h)
+        )
+
+    @classmethod
+    def from_mm(cls, x: float, y: float, w: float, h: float) -> 'CanvasBbox':
+        return CanvasBbox(
+            CanvasCoordinate.from_mm(x, y),
+            CanvasUnit.from_mm(w),
+            CanvasUnit.from_mm(h)
+        )
--- tests/canvas/test_canvas_bbox.py
+++ tests/canvas/test_canvas_bbox.py
@@ -11,3 +11,26 @@ class TestCanvasBbox(unittest.TestCase):
         assert bbox.pos == Cc.from_in(1, 2)
         assert bbox.width == Cu.from_in(3)
         assert bbox.height == Cu.from_in(4)
+
+    def test_identity(self):
+        assert CanvasBbox.from_pt(1, 2, 3, 4).pos.pt == (1, 2)
+        assert CanvasBbox.from_pt(1, 2, 3, 4).width.pt == 3
+        assert CanvasBbox.from_pt(1, 2, 3, 4).height.pt == 4
+
+        assert CanvasBbox.from_px(1, 2, 3, 4).pos.px == (1, 2)
+        assert CanvasBbox.from_px(1, 2, 3, 4).width.px == 3
+        assert CanvasBbox.from_px(1, 2, 3, 4).height.px == 4
+
+        assert CanvasBbox.from_in(1, 2, 3, 4).pos.inches == (1, 2)
+        assert CanvasBbox.from_in(1, 2, 3, 4).width.inches == 3
+        assert CanvasBbox.from_in(1, 2, 3, 4).height.inches == 4
+
+        assert CanvasBbox.from_mm(1, 2, 3, 4).pos.mm == (1, 2)
+        # Round to solve floating point issues
+        assert round(CanvasBbox.from_mm(1, 2, 3, 4).width.mm) == 3
+        assert CanvasBbox.from_mm(1, 2, 3, 4).height.mm == 4
+
+        assert CanvasBbox.from_cm(1, 2, 3, 4).pos.cm == (1, 2)
+        # Round to solve floating point issues
+        assert round(CanvasBbox.from_cm(1, 2, 3, 4).width.cm) == 3
+        assert CanvasBbox.from_cm(1, 2, 3, 4).height.cm == 4
