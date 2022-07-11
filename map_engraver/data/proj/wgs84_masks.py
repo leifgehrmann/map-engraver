86ba94f3d8652f137b3c0e562c6f8a36472b065f
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-07-11 20:48:29 +0100

    Fix WGS 84 mask - Got axis flipped incorrectly

--- map_engraver/data/proj/wgs84_masks.py
+++ map_engraver/data/proj/wgs84_masks.py
@@ -3,9 +3,9 @@ from shapely.geometry import Polygon
 
 def wgs84_mask() -> Polygon:
     return Polygon([
-        (-180, -90),
-        (-180, 90),
-        (180, 90),
-        (180, -90),
-        (-180, -90)
+        (-90, -180),
+        (-90, 180),
+        (90, 180),
+        (90, -180),
+        (-90, -180)
     ])
