6fce0121953c393e37980cc46b4830526ab483b2
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-07-14 22:23:34 +0100

    Add new homogenization method for LineString objects: geoms_to_multi_line_string

--- map_engraver/data/osm_shapely_ops/homogenize.py
+++ map_engraver/data/osm_shapely_ops/homogenize.py
@@ -1,6 +1,6 @@
 from typing import Union, List
 
-from shapely.geometry import MultiPolygon, GeometryCollection, Polygon
+from shapely.geometry import MultiPolygon, GeometryCollection, Polygon, MultiLineString, LineString
 
 
 def geoms_to_multi_polygon(
@@ -19,3 +19,21 @@ def geoms_to_multi_polygon(
         new_polygons.append(geoms)
 
     return MultiPolygon(new_polygons)
+
+
+def geoms_to_multi_line_string(
+        geoms: Union[LineString, MultiLineString, GeometryCollection]
+) -> MultiLineString:
+    new_line_strings: List[LineString] = []
+    if isinstance(geoms, GeometryCollection):
+        for inner_geom in geoms.geoms:
+            if isinstance(inner_geom, MultiLineString):
+                new_line_strings.extend(inner_geom.geoms)
+            elif isinstance(inner_geom, LineString):
+                new_line_strings.append(inner_geom)
+    elif isinstance(geoms, MultiLineString):
+        return geoms
+    elif isinstance(geoms, LineString):
+        new_line_strings.append(geoms)
+
+    return MultiLineString(new_line_strings)
