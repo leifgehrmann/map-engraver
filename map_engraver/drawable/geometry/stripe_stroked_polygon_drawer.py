0451d5073380772334a46e4ebc35a96496faf459
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-11-26 14:10:45 +0000

    Added Striped polygon drawer for drawing lines

--- map_engraver/drawable/geometry/stripe_stroked_polygon_drawer.py
+++ map_engraver/drawable/geometry/stripe_stroked_polygon_drawer.py
@@ -8,13 +8,14 @@ from map_engraver.canvas import Canvas
 from map_engraver.canvas.canvas_coordinate import CanvasCoordinate
 from map_engraver.canvas.canvas_unit import CanvasUnit
 from map_engraver.drawable.drawable import Drawable
-from map_engraver.drawable.geometry.polygon_drawer import PolygonDrawer
+from map_engraver.drawable.geometry.line_drawer import LineDrawer
 from map_engraver.drawable.geometry.stripe_utils import \
-    create_polygons_from_stripe_data
+    create_line_strings_from_stripe_data
 
 
-class StripeFilledPolygonDrawer(Drawable):
+class StripeStrokedPolygonDrawer(Drawable):
     stripe_widths: List[CanvasUnit]
+    stripe_stroke_widths: List[Optional[CanvasUnit]]
     stripe_colors: List[Optional[Tuple[float, float, float, float]]]
     stripe_origin: CanvasCoordinate
     stripe_angle: float
@@ -24,6 +25,7 @@ class StripeFilledPolygonDrawer(Drawable):
     def __init__(self):
         self.geoms = []
         self.stripe_widths = [CanvasUnit.from_pt(1)]
+        self.stripe_stroke_widths = [CanvasUnit.from_pt(1)]
         self.stripe_colors = [(0, 0, 0, 1)]
         self.stripe_angle = 0
         self.stripe_origin = CanvasCoordinate.origin()
@@ -33,23 +35,28 @@ class StripeFilledPolygonDrawer(Drawable):
             raise ValueError(
                 'length of stripe_widths is not the same as stripe_colors'
             )
+
+        if len(self.stripe_widths) != len(self.stripe_stroke_widths):
+            raise ValueError(
+                'length of stripe_widths is not the same as '
+                'stripe_stroke_widths'
+            )
+
         if len(self.stripe_widths) == 0:
             raise ValueError(
                 'stripe_widths must contain at least one value'
             )
-        if len(self.stripe_widths) == 1:
-            if self.stripe_colors[0] is None:
-                return
-            polygon_drawer = PolygonDrawer()
-            polygon_drawer.fill_color = self.stripe_colors[0]
-            polygon_drawer.geoms = self.geoms
-            polygon_drawer.draw(canvas)
-            return
 
         for stripe_width in self.stripe_widths:
             if stripe_width.pt <= 0:
                 raise ValueError(
                     'stripe_widths must be a positive non-zero length'
+                )
+
+        for stripe_stroke_width in self.stripe_stroke_widths:
+            if stripe_stroke_width is not None and stripe_stroke_width.pt <= 0:
+                raise ValueError(
+                    'stripe_stroke_widths must be a positive non-zero length'
                 )
 
         # Convert class-based inputs into floating point values.
@@ -61,15 +68,23 @@ class StripeFilledPolygonDrawer(Drawable):
             lambda stripe: stripe.pt,
             self.stripe_widths
         ))
+
+        def is_stripe_visible(stripe_info):
+            stripe_color = stripe_info[0]
+            stripe_stroke_width = stripe_info[1]
+            return stripe_color is not None and \
+                stripe_stroke_width is not None and \
+                stripe_stroke_width.pt > 0
+
         stripe_visible = list(map(
-            lambda stripe_color: stripe_color is not None,
-            self.stripe_colors
+            is_stripe_visible,
+            zip(self.stripe_colors, self.stripe_stroke_widths)
         ))
 
         # Create stripes out of every geom.
         geoms_per_stripe = [[] for _ in range(len(self.stripe_widths))]
         for geom in self.geoms:
-            new_geoms_per_stripe = create_polygons_from_stripe_data(
+            new_geoms_per_stripe = create_line_strings_from_stripe_data(
                 geom,
                 stripe_line,
                 stripe_widths_pt,
@@ -85,7 +100,8 @@ class StripeFilledPolygonDrawer(Drawable):
         for (stripe_index, geoms_for_stripe) in enumerate(geoms_per_stripe):
             if len(geoms_for_stripe) == 0:
                 continue
-            polygon_drawer = PolygonDrawer()
-            polygon_drawer.fill_color = self.stripe_colors[stripe_index]
-            polygon_drawer.geoms = geoms_per_stripe[stripe_index]
-            polygon_drawer.draw(canvas)
+            line_drawer = LineDrawer()
+            line_drawer.stroke_color = self.stripe_colors[stripe_index]
+            line_drawer.stroke_width = self.stripe_stroke_widths[stripe_index]
+            line_drawer.geoms = geoms_per_stripe[stripe_index]
+            line_drawer.draw(canvas)
--- tests/drawable/geometry/test_stripe_stroked_polygon_drawer.py
+++ tests/drawable/geometry/test_stripe_stroked_polygon_drawer.py
@@ -9,11 +9,11 @@ from shapely.geometry import Polygon, MultiPolygon
 from map_engraver.canvas import CanvasBuilder
 from map_engraver.canvas.canvas_coordinate import CanvasCoordinate
 from map_engraver.canvas.canvas_unit import CanvasUnit as Cu
-from map_engraver.drawable.geometry.stripe_filled_polygon_drawer import \
-    StripeFilledPolygonDrawer
+from map_engraver.drawable.geometry.stripe_stroked_polygon_drawer import \
+    StripeStrokedPolygonDrawer
 
 
-class TestStripeFilledPolygonDrawer(unittest.TestCase):
+class TestStripeStrokedPolygonDrawer(unittest.TestCase):
     def setUp(self):
         Path(__file__).parent.joinpath('output/')\
             .mkdir(parents=True, exist_ok=True)
@@ -23,32 +23,56 @@ class TestStripeFilledPolygonDrawer(unittest.TestCase):
                 Exception,
                 'length of stripe_widths is not the same as stripe_colors'
         ):
-            drawer = StripeFilledPolygonDrawer()
-            drawer.stripe_colors = [(0, 1, 0), (0, 1, 0)]
+            drawer = StripeStrokedPolygonDrawer()
+            drawer.stripe_colors = [(0, 1, 0), None]
             drawer.stripe_widths = [Cu.from_pt(10)]
+            drawer.stripe_stroke_widths = [Cu.from_pt(1)]
+            drawer.draw(MagicMock())
+
+        with self.assertRaisesRegex(
+                Exception,
+                'length of stripe_widths is not the same as '
+                'stripe_stroke_widths'
+        ):
+            drawer = StripeStrokedPolygonDrawer()
+            drawer.stripe_colors = [(0, 1, 0), (0, 1, 0)]
+            drawer.stripe_widths = [Cu.from_pt(10), Cu.from_pt(20)]
+            drawer.stripe_stroke_widths = [Cu.from_pt(1)]
             drawer.draw(MagicMock())
 
         with self.assertRaisesRegex(
                 Exception,
                 'stripe_widths must contain at least one value'
         ):
-            drawer = StripeFilledPolygonDrawer()
+            drawer = StripeStrokedPolygonDrawer()
             drawer.stripe_colors = []
             drawer.stripe_widths = []
+            drawer.stripe_stroke_widths = []
             drawer.draw(MagicMock())
 
         with self.assertRaisesRegex(
                 Exception,
                 'stripe_widths must be a positive non-zero length'
         ):
-            drawer = StripeFilledPolygonDrawer()
+            drawer = StripeStrokedPolygonDrawer()
             drawer.stripe_colors = [(0, 1, 0), (0, 1, 0)]
             drawer.stripe_widths = [Cu.from_pt(10), Cu.from_pt(0)]
+            drawer.stripe_stroke_widths = [Cu.from_pt(1), Cu.from_pt(2)]
+            drawer.draw(MagicMock())
+
+        with self.assertRaisesRegex(
+                Exception,
+                'stripe_stroke_widths must be a positive non-zero length'
+        ):
+            drawer = StripeStrokedPolygonDrawer()
+            drawer.stripe_colors = [(0, 1, 0), (0, 1, 0)]
+            drawer.stripe_widths = [Cu.from_pt(10), Cu.from_pt(10)]
+            drawer.stripe_stroke_widths = [Cu.from_pt(-1), Cu.from_pt(0)]
             drawer.draw(MagicMock())
 
     def test_single_stripe(self):
         path = Path(__file__).parent.joinpath(
-            'output/stripe_filled_polygon_drawer_single.svg'
+            'output/stripe_stroked_polygon_drawer_single.svg'
         )
         path.unlink(missing_ok=True)
         canvas_builder = CanvasBuilder()
@@ -57,8 +81,10 @@ class TestStripeFilledPolygonDrawer(unittest.TestCase):
 
         canvas = canvas_builder.build()
 
-        drawer = StripeFilledPolygonDrawer()
+        drawer = StripeStrokedPolygonDrawer()
         drawer.stripe_colors = [(0, 1, 0)]
+        drawer.stripe_widths = [Cu.from_pt(10)]
+        drawer.stripe_stroke_widths = [Cu.from_pt(1)]
         drawer.geoms = [
             Polygon([
                 (30, 30),
@@ -88,15 +114,17 @@ class TestStripeFilledPolygonDrawer(unittest.TestCase):
 
         with open(path, 'r') as file:
             data = file.read()
-            assert data.find('M 30 30 L 70 30 L 70 70 L 30 70 Z M 30 30') != -1
-            assert data.find('M 40 40 L 60 40 L 60 60 L 40 60 Z M 40 40') == -1
-            assert data.find('fill:rgb(0%,100%,0%)') != -1
-            assert data.find('stroke:none') != -1
-            assert data.find('stroke-width:') == -1
+            assert data.find('M 30 30 L 70 30') != -1
+            assert data.find('M 70 70 L 30 70') != -1
+            assert data.find('M 30 40 L 70 40') != -1
+            assert data.find('M 30 50 L 70 50') != -1
+            assert data.find('M 30 60 L 70 60') != -1
+            assert data.count('stroke:rgb(0%,100%,0%)') == 5
+            assert data.count('stroke-width:1') == 5
 
     def test_two_vertical_stripes(self):
         path = Path(__file__).parent.joinpath(
-            'output/stripe_filled_polygon_drawer_two_vertical_stripes.svg'
+            'output/stripe_stroked_polygon_drawer_two_vertical_stripes.svg'
         )
         path.unlink(missing_ok=True)
         canvas_builder = CanvasBuilder()
@@ -105,9 +133,10 @@ class TestStripeFilledPolygonDrawer(unittest.TestCase):
 
         canvas = canvas_builder.build()
 
-        drawer = StripeFilledPolygonDrawer()
+        drawer = StripeStrokedPolygonDrawer()
         drawer.stripe_colors = [(0, 1, 0), (0, 0, 1)]
         drawer.stripe_widths = [Cu.from_pt(20), Cu.from_pt(20)]
+        drawer.stripe_stroke_widths = [Cu.from_pt(1), Cu.from_pt(2)]
         drawer.stripe_origin = CanvasCoordinate.from_pt(30, 30)
         drawer.stripe_angle = math.pi / 2  # Vertical stripes
         drawer.geoms = [
@@ -127,16 +156,16 @@ class TestStripeFilledPolygonDrawer(unittest.TestCase):
 
         with open(path, 'r') as file:
             data = file.read()
-            assert data.find('M 30 30 L 30 70 L 50 70 L 50 30 Z M 30 30') != -1
-            assert data.find('M 50 30 L 50 70 L 70 70 L 70 30 Z M 50 30') != -1
-            assert data.find('fill:rgb(0%,100%,0%)') != -1
-            assert data.find('fill:rgb(0%,0%,100%)') != -1
-            assert data.find('stroke:none') != -1
-            assert data.find('stroke-width:') == -1
+            assert data.find('M 30 70 L 30 30') != -1
+            assert data.find('M 50 30 L 50 70') != -1
+            assert data.find('stroke:rgb(0%,100%,0%)') != -1
+            assert data.find('stroke:rgb(0%,0%,100%)') != -1
+            assert data.find('stroke-width:1') != -1
+            assert data.find('stroke-width:2') != -1
 
     def test_large_stripes(self):
         path = Path(__file__).parent.joinpath(
-            'output/stripe_filled_polygon_drawer_large_stripes.svg'
+            'output/stripe_stroked_polygon_drawer_large_stripes.svg'
         )
         path.unlink(missing_ok=True)
         canvas_builder = CanvasBuilder()
@@ -145,9 +174,10 @@ class TestStripeFilledPolygonDrawer(unittest.TestCase):
 
         canvas = canvas_builder.build()
 
-        drawer = StripeFilledPolygonDrawer()
+        drawer = StripeStrokedPolygonDrawer()
         drawer.stripe_colors = [(0, 1, 0), (0, 0, 1)]
-        drawer.stripe_widths = [Cu.from_pt(100), Cu.from_pt(20)]
+        drawer.stripe_widths = [Cu.from_pt(7), Cu.from_pt(7)]
+        drawer.stripe_stroke_widths = [Cu.from_pt(1), Cu.from_pt(2)]
         drawer.stripe_origin = CanvasCoordinate.from_pt(30, 30)
         drawer.stripe_angle = -math.pi / 2  # Vertical stripes
         drawer.geoms = [
@@ -167,15 +197,19 @@ class TestStripeFilledPolygonDrawer(unittest.TestCase):
 
         with open(path, 'r') as file:
             data = file.read()
-            assert data.find('M 30 30 L 30 70 L 70 70 L 70 30 Z M 30 30') != -1
-            assert data.find('fill:rgb(0%,100%,0%)') != -1
-            assert data.find('fill:rgb(0%,0%,100%)') == -1
-            assert data.find('stroke:none') != -1
-            assert data.find('stroke-width:') == -1
+            assert data.find('M 44 70 L 44 30') != -1
+            assert data.find('M 58 70 L 58 30') != -1
+            assert data.find('M 37 70 L 37 30') != -1
+            assert data.find('M 51 70 L 51 30') != -1
+            assert data.find('M 65 70 L 65 30') != -1
+            assert data.find('stroke:rgb(0%,100%,0%)') != -1
+            assert data.find('stroke:rgb(0%,0%,100%)') != -1
+            assert data.find('stroke:rgb(0%,100%,0%)') < \
+                   data.find('stroke:rgb(0%,0%,100%)')
 
     def test_multi_polygons(self):
         path = Path(__file__).parent.joinpath(
-            'output/stripe_filled_polygon_drawer_multi_polygons.svg'
+            'output/stripe_stroked_polygon_drawer_multi_polygons.svg'
         )
         path.unlink(missing_ok=True)
         canvas_builder = CanvasBuilder()
@@ -184,10 +218,13 @@ class TestStripeFilledPolygonDrawer(unittest.TestCase):
 
         canvas = canvas_builder.build()
 
-        drawer = StripeFilledPolygonDrawer()
+        drawer = StripeStrokedPolygonDrawer()
         drawer.stripe_colors = [(1, 1, 0), None, (0.7, 0, 0.7), (0, 0, 0)]
         drawer.stripe_widths = [
             Cu.from_pt(5), Cu.from_pt(5), Cu.from_pt(5), Cu.from_pt(5),
+        ]
+        drawer.stripe_stroke_widths = [
+            Cu.from_pt(1), None, Cu.from_pt(3), Cu.from_pt(4)
         ]
         drawer.stripe_origin = CanvasCoordinate.from_pt(30, 30)
         drawer.stripe_angle = math.pi / 8  # Vertical stripes
@@ -223,8 +260,9 @@ class TestStripeFilledPolygonDrawer(unittest.TestCase):
 
         with open(path, 'r') as file:
             data = file.read()
-            assert data.find('fill:rgb(100%,100%,0%)') != -1
-            assert data.find('fill:rgb(0%,0%,0%)') != -1
-            assert data.find('fill:rgb(70%,0%,70%)') != -1
-            assert data.find('stroke:none') != -1
-            assert data.find('stroke-width:') == -1
+            assert data.count('stroke:rgb(100%,100%,0%)') == 6
+            assert data.count('stroke:rgb(0%,0%,0%)') == 6
+            assert data.count('stroke:rgb(70%,0%,70%)') == 6
+            assert data.count('stroke-width:1') == 6
+            assert data.count('stroke-width:3') == 6
+            assert data.count('stroke-width:4') == 6
